dcdfc0e272e45958b496a51f7a20fade0ee82b90
oscarkd <okdangle@gmail.com>
2021-01-24 17:22:33 -0800

    lab 4 work

---
+++ lab4/js/lab.js
@@ -0,0 +1,29 @@
+// Loop 3 times
+    // Ask the user to input rock, paper, or scissors
+        // Validate input
+    // The computer selects random choice
+    // Compare Choices
+        // If the player selects rock
+            // If the computer selects rock
+                // Declare tie
+            // If the computer selects paper
+                // Declare loss
+            // If the computer selects scissors
+                // Declare win
+        // If the player selects paper
+            // If the computer selects rock
+                // Declare win
+            // If the computer selects paper
+                // Declare tie
+            // If the computer selects scissors
+                // Declare loss
+        // If the player selects scissors
+            // If the computer selects rock
+                // Declare loss
+            // If the computer selects paper
+                // Declare win
+            // If the computer selects scissors
+                // Declare tie
+// Count the number of Player wins
+  // If the player has more wins than the computer, declare the player the winner
+  // If the computer has more wins, tell the player they lost
