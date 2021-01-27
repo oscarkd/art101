3cd427316ee2ddbd7ff19d39f7c93c8e2b2584fe
oscarkd <okdangle@gmail.com>
2021-01-27 15:50:59 -0800

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+/*
+* Author: Oscar Kim-Dangle <okimdang@ucsc.edu>
+* Created: 27 January 2021
+* License: Student
+*/
+
+//Variables
+var make = "tesla";
+var model = "flymobile";
+var color = "silver";
+var year = "1999";
+var ownit = false;
+
+//Calulate age of car
+var age = 2021 - year;
+
+// output
+document.writeln("Make: "+make+"<br>");
+document.writeln("Model: "+model+"<br>");
+document.writeln("Color: "+color+"<br>");
+document.writeln("Year: "+year+"<br>");
+document.writeln("Do I own this car? "+ownit+"<br>");
+document.writeln("Car Age: "+age+" years<br>");
